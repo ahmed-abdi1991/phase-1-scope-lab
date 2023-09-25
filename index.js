8d240948866d57d29c9c5a2eb2750982f685d21f
ahmed-abdi1991 <squarehajji@gmail.com>
2023-09-25 07:26:47 +0300

    assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+// Declare customerName in the global scope
+var customerName = "bob";
+
+// Create a function to return customerName
+function returnCustomerName() {
+  return customerName;
+}
+// Declare customerName in the global scope
+var customerName = "bob";
+
+// Create a function to modify customerName to uppercase
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+// Declare the bestCustomer variable in the global scope
+var bestCustomer;
+
+// Declare the bestCustomer variable in the global scope
+var bestCustomer;
+
+// Create a function to set the bestCustomer variable
+function setBestCustomer() {
+  bestCustomer = "John";
+}
+
+// Declare the bestCustomer variable in the global scope
+var bestCustomer = "Initial Customer";
